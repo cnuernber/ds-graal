b1c0c287b346a49294563c550faaafbefd66f551
Chris Nuernberger <chris@techascent.com>
2021-04-08 17:07:29 -0600

    Initial commit.

---
+++ src/ds_graal/main.clj
@@ -0,0 +1,15 @@
+(ns ds-graal.main
+  (:require [tech.v3.dataset :as ds]
+            [tech.v3.datatype.functional :as dfn]
+            [tech.v3.libs.parquet :as parquet])
+  (:gen-class))
+
+
+(defn -main
+  [& args]
+  (let [test-ds (ds/->dataset "test/data/stocks.csv" {:key-fn keyword})]
+    (println test-ds)
+    (println "price mean:" (dfn/mean (test-ds :price)))
+    (parquet/ds->parquet test-ds "stocks.parquet")
+    (println "succesfully wrote stocks.parquet")
+    0))
